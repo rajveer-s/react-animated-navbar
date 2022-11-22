f56b09ef1ee2d743a41ad31a116b4a06cddaf6e6
Rajveer Singh <rajsidhu2924@gmail.com>
2022-11-22 13:37:24 -0800

    navbar overlay created

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Navbar = () => {
+
+  const [navOpen, setNavOpen] = useState(false);
+
+
   return (
-    <div>Navbar</div>
+    <div className="nav">
+      <div className="nav-container">
+        <div className="navbar">
+          <div className="logo">rajveer.</div>
+          <div className="menu-toggle" onClick={() => setNavOpen(!navOpen)}>
+            <div className={navOpen ? "hamBox hamBoxOpen" : "hamBox"}>
+              <span className={navOpen ? "lineTop spin" : "lineTop"}></span>
+              <span
+                className={navOpen ? "lineBottom spin" : "lineBottom"}
+              ></span>
+            </div>
+          </div>
+        </div>
+        <div
+          className="nav-overlay"
+          style={{
+            top: navOpen ? "0" : "-100%",
+            transitionDelay: navOpen ? "0s" : "0s"
+          }}
+        >
   )
 }
 
