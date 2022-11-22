e4e843ffb096a11381156079c619e8e214a01258
Rajveer Singh <rajsidhu2924@gmail.com>
2022-11-22 13:38:57 -0800

    Links created to go diffrent routes with react-router-dom

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -26,7 +26,74 @@ const Navbar = () => {
             transitionDelay: navOpen ? "0s" : "0s"
           }}
         >
-  )
+          <ul className="nav-links">
+            <li className="nav-item">
+              <Link
+                to="/"
+                onClick={() => setNavOpen(!navOpen)}
+                style={{
+                  top: navOpen ? "0" : "120px",
+                  transitionDelay: navOpen ? "0.8s" : "0s"
+                }}
+              >
+                About
+              </Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/"
+                onClick={() => setNavOpen(!navOpen)}
+                style={{
+                  top: navOpen ? "0" : "120px",
+                  transitionDelay: navOpen ? "1.1s" : "0s"
+                }}
+              >
+                Skills
+              </Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/"
+                onClick={() => setNavOpen(!navOpen)}
+                style={{
+                  top: navOpen ? "0" : "120px",
+                  transitionDelay: navOpen ? "1.1s" : "0s"
+                }}
+              >
+                Work
+              </Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/"
+                onClick={() => setNavOpen(!navOpen)}
+                style={{
+                  top: navOpen ? "0" : "120px",
+                  transitionDelay: navOpen ? "1.1s" : "0s"
+                }}
+              >
+                Contact
+              </Link>
+              <div className="nav-item-wrapper"></div>
+            </li>
+          </ul>
+          <div className="nav-footer">
+            <div
+              className="location"
+              style={{
+                bottom: navOpen ? "0" : "-20px",
+                opacity: navOpen ? "1" : "0",
+                transitionDelay: navOpen ? "1.2s" : "0s"
+              }}
+            >
+              <span>Los Angeles, CA</span>
+            </div>
+            <div className="nav-social-media">
+              <ul></ul>
+              )
 }
 
-export default Navbar
+              export default Navbar
